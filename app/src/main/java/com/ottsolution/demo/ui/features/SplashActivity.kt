4c3c2bf6fe50bf7d27a669313bffe6cc9d51823f
shilpi.mangal <shilpi.mangal@tothenew.com>
2019-08-21 11:56:39 +0530

    Splash video view added

--- app/src/main/java/com/ottsolution/demo/ui/features/SplashActivity.kt
+++ app/src/main/java/com/ottsolution/demo/ui/features/SplashActivity.kt
@@ -1,22 +1,40 @@
 package com.ottsolution.demo.ui.features
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.ottsolution.demo.R
 import com.ottsolution.demo.ui.features.home.HomeActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
-
-    private val mHandler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        mHandler.postDelayed({
-            startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
-            finish()
-        }, 1000)
+
+        initView()
+
+    }
+
+    private fun initView() {
+        try {
+            videoView.setMediaController(null)
+            videoView.setOnCompletionListener({ onCompletedCheck() })
+
+            val path = "android.resource://" + getPackageName() + "/" + R.raw.splash
+            videoView.setVideoURI(Uri.parse(path))
+            videoView.start()
+        } catch (e: Exception) {
+            Log.e("", e.message, e)
+        }
+
+    }
+
+    private fun onCompletedCheck() {
+        startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
+        finish()
     }
 }
